0a2f91aa1eb134b8b5d7f4d3465d4406209d5683
syd1 <syding036@gmail.com>
2025-12-13 22:01:10 -0500

    MCTS seems to be working

--- othello-mcts-seq.hs
+++ othello-mcts-seq.hs
@@ -1,23 +1,12 @@
-{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
-{-# HLINT ignore "Use if" #-}
-import System.IO
-import Data.Int
-import GHC.Base (VecElem(Int64ElemRep))
-import GHC.Arr
-
-import System.Random (split, StdGen)
-import System.Posix.Internals (puts)
-import Data.List (maximumBy, minimumBy)
+import System.Random (StdGen, newStdGen, randomR)
+import Data.List (maximumBy)
 import Data.Ord (comparing)
-import Debug.Trace (trace)
-import System.Random(StdGen, newStdGen, randomR)
 
 {-
 Commands to compile and run this program:
 stack install random
 TODO: set it up as stack project with proper .yaml files in the long run to make compilations easier
 For now, run the following command in terminal to compile and run:
-stack ghc --package random -- -o othello-mcts-seq othello-mcts-seq.hs
 stack ghc  --package random  -- -Wall -O2 -o othello-mcts-seq othello-mcts-seq.hs
 ./othello-mcts-seq
 -}
@@ -25,12 +14,7 @@ stack ghc  --package random  -- -Wall -O2 -o othello-mcts-seq othello-mcts-seq.h
 -- Game types
 type Position = (Int, Int)
 
-type Board = [[Int]]
-
-type Player = Int -- 0 means not taken, 1 means taken by player 1, 2 means taken by player 2
-
-type PDisks = [Position]
-
+type Board = [[Int]] -- 0: empty, 1: player 1's disk, 2: player 2's disk
 
 data BoardState = BoardState {
     board       :: Board,
@@ -49,17 +33,20 @@ cols = 8
 gameBoard :: Board
 gameBoard = [[0 | _ <- [1..cols]] | _ <- [1..rows]] :: [[Int]]
 
-
 updateBoardIndex :: Board -> Position -> Int -> Board
-updateBoardIndex board (i,j) val = prevRows ++ [updatedRow] ++ afterRows where
-    (prevRows, currRow : afterRows) = splitAt i board
-    (prevElems, currElem : afterElems) = splitAt j currRow
-    updatedRow = prevElems ++ [val] ++ afterElems
+updateBoardIndex board (i,j) val = 
+    case splitAt i board of
+        (prevRows, currRow : afterRows) ->
+            case splitAt j currRow of
+                (prevElems, _ : afterElems) -> 
+                    prevRows ++ [prevElems ++ [val] ++ afterElems] ++ afterRows
+                (_, _) -> board
+        (_, _) -> board
 
 updateBoardIndexes :: Board -> [Position] -> [Int] -> Board
-updateBoardIndexes board [] [] = board
 updateBoardIndexes board (x:xs) (y:ys) = updateBoardIndexes newBoard xs ys where
     newBoard = updateBoardIndex board x y
+updateBoardIndexes board _ _ = board
 
 {-
 Initialize the gameboard to start configuration and set current player to 1
@@ -72,9 +59,10 @@ initializeBoard = BoardState {
 
 {-
 Printing for Board
+Commented out because noPrintGameLoop will not print boards
 -}
-printBoard :: Board -> IO ()
-printBoard board = mapM_ (putStrLn . unwords . map show) board
+-- printBoard :: Board -> IO ()
+-- printBoard board = mapM_ (putStrLn . unwords . map show) board
 
 
 {-
@@ -111,7 +99,7 @@ Return a list of directions where both conditions are met
 If the list is non-empty, then the position is a valid move that satisfy both adjancy and flankinng rules
 -}
 adjacencyAndFlankingCheck :: BoardState -> Position -> [Position]
-adjacencyAndFlankingCheck bs (i,j) = [ d | d@(di,dj) <- directions, isAdjacentToOpp d && flankOpp d]
+adjacencyAndFlankingCheck bs (i,j) = [ d | d <- directions, isAdjacentToOpp d && flankOpp d]
     where
         b = board bs
         curPlayer = curr_player bs
@@ -135,13 +123,6 @@ adjacencyAndFlankingCheck bs (i,j) = [ d | d@(di,dj) <- directions, isAdjacentTo
                         val | val == curPlayer -> True
                         val | val == oppPlayer -> scan (x + di) (y + dj)
                         _ -> False
-
-switchPlayer :: BoardState -> BoardState
-switchPlayer bs = new_bs where
-    new_bs = BoardState {
-        board = board bs,
-        curr_player = if curr_player bs == 1 then 2 else 1
-    }
 
 {-
 Given the position of the to-be-placed disc of current player and BoardState, update the discs on board and change curr_player to opponent player
@@ -191,45 +172,6 @@ checkWinner bs
         p1Count = countDisc bs 1
         p2Count = countDisc bs 2
 
-{-
-Given a BoardState, return heuristic score of board
--}
--- TODO
-evaluateBoard :: BoardState -> Int
-evaluateBoard bs = 2*(cornerHeuristic bs) + (mobilityHeuristic bs) + (discCountHeuristic bs)
-
-
-{-
-Given a BoardState, return higher scores if current player has corners
--}
-
-cornerHeuristic :: BoardState -> Int
-cornerHeuristic bs = c1 + c2 + c3 + c4 where
-    b = board bs
-    curPlayer = curr_player bs
-    c1 = if getBoardVal b (0,0) == curPlayer then 2 else 0
-    c2 = if getBoardVal b (0,7) == curPlayer then 2 else 0
-    c3 = if getBoardVal b (7,0) == curPlayer then 2 else 0
-    c4 = if getBoardVal b (7,7) == curPlayer then 2 else 0
-
-{-
-Given a BoardState, return higher scores if current player has more moves
--}
-mobilityHeuristic :: BoardState -> Int
-mobilityHeuristic bs = length pos where
-    pos = getPossibleMoves bs
-
-{-
-Given a Board state, return higher score if current player has more discs on board by taking this move
--}
-discCountHeuristic :: BoardState -> Int
-discCountHeuristic bs = (playerCount - oppCount) `div` 3
-    where
-        curPlayer = curr_player bs
-        oppPlayer = if curPlayer == 1 then 2 else 1
-        playerCount = countDisc bs curPlayer
-        oppCount = countDisc bs oppPlayer
-
 -- MCTS related functions start here
 -- Tutorial on MCTS, where the following code snippets are based off: https://www.youtube.com/watch?v=UXW2yZndl7U&t=1s
 {-
@@ -249,7 +191,7 @@ Calculate UCT value for a given MCTSNode
 uctValue :: MCTSNode -> Int -> Double
 uctValue node parentN =
     if n_i node == 0 then 1/0 -- infinity
-    else (t node / fromIntegral (n_i node)) + 2 * sqrt (log (fromIntegral parentN) / fromIntegral (n_i node))
+    else (t node / fromIntegral (n_i node)) + (sqrt 2) * sqrt (log (fromIntegral parentN) / fromIntegral (n_i node))
 
 {-
 Selection: select the best child to perform simulation based on UCT values
@@ -295,25 +237,24 @@ t will be set to be heuristic score of the BoardState reached at terminal state,
 simulation :: MCTSNode -> IO Double
 simulation node = do
     gen <- newStdGen
-    let rootPlayer = curr_player (state node)
-    return $ simulateFromState (state node) gen rootPlayer
+    return $ simulateFromState (state node) gen
 
-simulateFromState :: BoardState -> StdGen -> Int -> Double
-simulateFromState bs g rootPlayer =
+simulateFromState :: BoardState -> StdGen -> Double
+simulateFromState bs g =
     let moves = getPossibleMoves bs
     in if null moves
-        then evaluateBoardMTCS bs rootPlayer
+        then evaluateBoardMTCS bs
         else
             let (randomIndex, g') = randomR (0, length moves - 1) g
                 move = moves !! randomIndex
                 newBs = updateTurn move bs
-            in simulateFromState newBs g' rootPlayer
+            in simulateFromState newBs g'
 
-evaluateBoardMTCS :: BoardState -> Int -> Double
-evaluateBoardMTCS bs rootPlayer =
-    case checkWinner bs of
-        0 -> 0.0
-        winner -> if winner == rootPlayer then 1.0 else -1.0
+evaluateBoardMTCS :: BoardState -> Double
+evaluateBoardMTCS bs = fromIntegral (p2Count - p1Count)
+    where
+        p1Count = countDisc bs 1
+        p2Count = countDisc bs 2
 
 {-
 Backpropagation: propagate n_i and t values along the path from selected node back to root
@@ -347,10 +288,14 @@ runMCTSIteration root = do
     let (selectedNode, path, _) = selection root []
     case m selectedNode of
         Nothing -> return root -- Root it selected, try expanding from the root
-        Just pos -> do  -- See if we can try expansion
+        Just _ -> do  -- See if we can try expansion
             let possibleMoves = getPossibleMoves (state selectedNode)
             if null possibleMoves
-            then return root -- No possible moves to expand
+            then do
+                -- Terminal node: still run simulation and backpropagation to update n_i and t values
+                reward <- simulation selectedNode
+                let updatedRoot = backpropagation (reverse path) selectedNode reward
+                return updatedRoot
             else do 
                 gen <- newStdGen
                 let (randomIndex, _) = randomR (0, length possibleMoves - 1) gen
@@ -358,52 +303,11 @@ runMCTSIteration root = do
                 let (expandedNode, potentialChild) = expansion selectedNode move -- 2. Expansion
                 case potentialChild of
                     Nothing             -> return root -- we could not successfull expand
-                    Just potentialChild -> do
-                        reward <- simulation potentialChild -- 3. Simulation
+                    Just pc -> do
+                        reward <- simulation pc -- 3. Simulation
                         -- expandedNode is the parent of the potentialChild
-                        let updatedRoot = backpropagation (expandedNode : reverse path) potentialChild reward
+                        let updatedRoot = backpropagation (expandedNode : reverse path) pc reward
                         return updatedRoot
-
-{-
-Trying a different implementation of MCTS
-Based on youtube video, we only do an expansion if the n_i value of a leaf != 0
--}
-
--- runMCTSIteration :: MCTSNode -> IO MCTSNode
--- runMCTSIteration root = do
---     -- 1. selection, select the best leaf or unexlpored, starting from the root
---     let (selectedNode, path, needToExpand) = selection root []
---     case needToExpand of
---         True -> do -- need to expand first
---             gen <- newStdGen
---             let possibleMoves = getPossibleMoves (state selectedNode)
---             let (randomIndex, _) = randomR (0, length possibleMoves - 1) gen
---             let move = possibleMoves !! randomIndex
---             let (expandedNode, potentialChild) = expansion selectedNode move -- 2. Expansion
---             case potentialChild of
---                 Nothing             -> return root -- we could not successfull expand
---                 Just potentialChild -> do
---                     reward <- simulation potentialChild -- 3. Simulation
---                     -- expandedNode is the parent of the potentialChild
---                     let updatedRoot = backpropagation (expandedNode : reverse path) potentialChild reward
---                     return updatedRoot
---         False -> do -- can directly rollout
---             reward <- simulation selectedNode -- 3. Simulation
---             let updatedRoot = backpropagation (reverse path) selectedNode reward
---             return updatedRoot
-
-
-{-
-Add child node given the tree root, path to parent, and the new child node
-Return the child in the copied tree structure
--}
-addChildMCTSNode :: MCTSNode -> [Maybe Position] -> MCTSNode -> MCTSNode
-addChildMCTSNode parent [] child = parent {children = child : children parent}  
-addChildMCTSNode node (curr_move : rest_moves) newChild = node {children = updatedChildren} where
-    updatedChildren = map updateHelper (children node) -- call updateHelper on all of the children of current node
-    updateHelper child = if m child == curr_move 
-                                    then addChildMCTSNode child rest_moves newChild
-                                    else child
 
 {-
 Run MCTS for a given number of iterations starting from the given root node
@@ -425,13 +329,82 @@ runMCTS root n = do
 
 {-
 gameLoop logic for alternating between players
-TODO: need to implement alpha-beta pruning later to optimize miniMax
--}
--- To illustrate that the miniMax is working, we make player 2 use miniMax to pick its moves
+-- To illustrate that the MCTS is working, we make player 2 use miniMax to pick its moves
 -- For now, player 1 will just pick a random vailable move
 -- We should be able to see player 2 winning more often than player 1
-gameLoop :: BoardState -> IO ()
-gameLoop bs = do
+-}
+-- gameLoop :: BoardState -> IO ()
+-- gameLoop bs = do
+--     let possibleMoves = getPossibleMoves bs
+--     if null possibleMoves
+--         then do
+--             let oppPlayer = if curr_player bs == 1 then 2 else 1
+--                 oppPossibleMoves = getPossibleMoves (BoardState { board = board bs, curr_player = oppPlayer })
+--             if null oppPossibleMoves
+--                 then do
+--                     let winner = checkWinner bs
+--                     if winner == 0
+--                     then putStrLn "Game over! It's a tie!"
+--                     else do
+--                         putStrLn $ "Game over! Winner is Player " ++ show winner
+--                         -- putStrLn $ "Final Board:"
+--                         -- printBoard (board bs)
+--                         putStrLn $ "Player 1 discs: " ++ show (countDisc bs 1)
+--                         putStrLn $ "Player 2 discs: " ++ show (countDisc bs 2)
+--                 else do
+--                     putStrLn $ "Player " ++ show (curr_player bs) ++ " has no moves. Skipping turn."
+--                     gameLoop (BoardState { board = board bs, curr_player = oppPlayer })
+--     else do
+--         -- putStrLn $ "Possible moves for Player " ++ show (curr_player bs) ++ ": " ++ show possibleMoves
+--         move <- if curr_player bs == 1
+--                     then do
+--                         -- Pick a random move for player 1
+--                         gen <- newStdGen
+--                         let (randomIndex, _) = randomR (0, length possibleMoves - 1) gen
+--                         let move = possibleMoves !! randomIndex
+--                         -- putStrLn $ "Player 1 (Random) chooses move " ++ show move
+--                         return move
+--                     else do
+--                         -- putStrLn $ "Player 2 (MCTS) chooses move "
+--                         let rootNode = MCTSNode {
+--                             state = bs,
+--                             children = [ c | mv <- possibleMoves,
+--                                             let bsNew = updateTurn mv bs,
+--                                             let c = MCTSNode {
+--                                                 state = bsNew,
+--                                                 children = [],
+--                                                 n_i = 0,
+--                                                 t = 0.0,
+--                                                 m = Just mv
+--                                             }
+--                                             ],
+--                             n_i = 0,
+--                             t = 0.0,
+--                             m = Nothing
+--                         }
+--                         mctsRoot <- runMCTS rootNode 1000 -- Run MCTS for 1000 iterations
+--                         -- Print statements to make sure the MTCS actually works and results are propagated back to root
+--                         putStrLn $ "Possible moves for Player 2 " ++ show (curr_player bs) ++ ": " ++ show possibleMoves
+--                         putStrLn $ "MCTS Root after iterations: " ++ show mctsRoot
+--                         let bestChild = maximumBy (comparing (\child -> uctValue child (n_i mctsRoot))) (children mctsRoot)
+--                         let move = case m bestChild of
+--                                         Just pos -> pos
+--                                         Nothing -> error "No move found. Something went wrong with MCTS implementation."
+--                         -- putStrLn $ "Player 2 (MCTS) chooses move " ++ show move
+--                         return move
+--         -- putStrLn $ "Player " ++ show (curr_player bs) ++ " places disc at " ++ show move
+--         -- putStrLn "BEFORE MOVE:"
+--         -- printBoard (board bs)
+--         let newGameState = updateTurn move bs
+--         -- putStrLn "AFTER MOVE:"
+--         -- printBoard (board newGameState)
+--         gameLoop newGameState
+
+{-
+noPrintGameLoop logic for alternating between players without printing boards
+-}
+noPrintGameLoop :: BoardState -> IO ()
+noPrintGameLoop bs = do
     let possibleMoves = getPossibleMoves bs
     if null possibleMoves
         then do
@@ -444,25 +417,20 @@ gameLoop bs = do
                     then putStrLn "Game over! It's a tie!"
                     else do
                         putStrLn $ "Game over! Winner is Player " ++ show winner
-                        putStrLn $ "Final Board:"
-                        printBoard (board bs)
                         putStrLn $ "Player 1 discs: " ++ show (countDisc bs 1)
                         putStrLn $ "Player 2 discs: " ++ show (countDisc bs 2)
                 else do
                     putStrLn $ "Player " ++ show (curr_player bs) ++ " has no moves. Skipping turn."
-                    gameLoop (BoardState { board = board bs, curr_player = oppPlayer })
+                    noPrintGameLoop (BoardState { board = board bs, curr_player = oppPlayer })
     else do
-        putStrLn $ "Possible moves for Player " ++ show (curr_player bs) ++ ": " ++ show possibleMoves
         move <- if curr_player bs == 1
                     then do
                         -- Pick a random move for player 1
                         gen <- newStdGen
                         let (randomIndex, _) = randomR (0, length possibleMoves - 1) gen
                         let move = possibleMoves !! randomIndex
-                        putStrLn $ "Player 1 (Random) chooses move " ++ show move
                         return move
                     else do
-                        putStrLn $ "Player 2 (MCTS) chooses move "
                         let rootNode = MCTSNode {
                             state = bs,
                             children = [ c | mv <- possibleMoves,
@@ -475,32 +443,23 @@ gameLoop bs = do
                                                 m = Just mv
                                             }
                                             ],
-
                             n_i = 0,
                             t = 0.0,
                             m = Nothing
                         }
-                        mctsRoot <- runMCTS rootNode 1 -- Run MCTS for 100 iterations
-                        -- Print statements to make sure the MTCS actually works and results are propagated back to root
-                        putStrLn $ "MCTS Root after iterations: " ++ show mctsRoot
+                        mctsRoot <- runMCTS rootNode 10000 -- Run MCTS for 10000 iterations
                         let bestChild = maximumBy (comparing (\child -> uctValue child (n_i mctsRoot))) (children mctsRoot)
                         let move = case m bestChild of
                                         Just pos -> pos
                                         Nothing -> error "No move found. Something went wrong with MCTS implementation."
-                        putStrLn $ "Player 2 (MCTS) chooses move " ++ show move
                         return move
-        putStrLn $ "Player " ++ show (curr_player bs) ++ " places disc at " ++ show move
-        putStrLn "BEFORE MOVE:"
-        printBoard (board bs)
         let newGameState = updateTurn move bs
-        putStrLn "AFTER MOVE:"
-        printBoard (board newGameState)
-        gameLoop newGameState
+        noPrintGameLoop newGameState
 
 main :: IO ()
 main = do
     -- gameLoop initializeBoard
-    gameLoop initializeBoard
+    noPrintGameLoop initializeBoard
     putStrLn "Thanks for playing!"
     -- Simple testing for heuristics, can delete later
 
